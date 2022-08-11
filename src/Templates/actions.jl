3412de1473ec885cae62b625923e892b5cfd12c8
Reno <singularitti@outlook.com>
2022-08-11 00:15:41 -0400

    Add type `DownloadPotentials` & method `buildjob` (`jobify`)

--- src/Templates/actions.jl
+++ src/Templates/actions.jl
@@ -1,11 +1,18 @@
 using AbInitioSoftwareBase: save, load, extension
 using AbInitioSoftwareBase.Inputs: Input, getpseudodir, getpotentials
-using Pseudopotentials: download_potential
-
 using ExpressBase: Action, calculation
+using Pseudopotentials: download_potential
+using SimpleWorkflows: Job
+
+using ..Config: ConfigFile
+
+export DownloadPotentials
+export jobify
+
+struct ExpandConfig{T} <: Action{T} end  # To be extended
 
 struct DownloadPotentials{T} <: Action{T} end
-function (x::DownloadPotentials)(template::Input)
+function (::DownloadPotentials)(template::Input)
     dir = getpseudodir(template)
     if !isdir(dir)
         mkpath(dir)
@@ -17,4 +24,12 @@ function (x::DownloadPotentials)(template::Input)
             download_potential(potential, path)
         end
     end
+end
+
+jobify(x::DownloadPotentials, template::Input) = Job(() -> x(template))
+jobify(x::DownloadPotentials, config) = Job(() -> x(config.template))
+function jobify(x::DownloadPotentials{T}, file::ConfigFile) where {T}
+    raw_config = load(file)
+    config = ExpandConfig{T}()(raw_config)
+    return jobify(x, config)
 end
