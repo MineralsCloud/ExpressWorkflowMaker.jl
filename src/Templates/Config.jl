359cf87400e7036bc12e60532fd3f266189b35fc
Reno <singularitti@outlook.com>
2022-08-12 06:17:42 -0400

    Add iteration & array interfaces to `VectorOption`

--- src/Templates/Config.jl
+++ src/Templates/Config.jl
@@ -1,7 +1,7 @@
 module Config
 
 using Configurations: OptionField, @option
-using Unitful: Unitful, FreeUnits, uparse
+using Unitful: Unitful, FreeUnits, Quantity, uparse, dimension
 using UnitfulAtomic: UnitfulAtomic
 
 import Configurations: from_dict
@@ -44,4 +44,24 @@ from_dict(
     ::Type{<:FreeUnits},
     str::AbstractString,
 ) = _uparse(str)
+
+# Similar to https://github.com/JuliaCollections/IterTools.jl/blob/0ecaa88/src/IterTools.jl#L1028-L1032
+function Base.iterate(iter::VectorOption, state = 1)
+    if state > length(iter.values)
+        return nothing
+    else
+        return getindex(iter.values, state) * iter.unit, state + 1
+    end
+end
+
+Base.eltype(iter::VectorOption) = Quantity{Float64,dimension(iter.unit),typeof(iter.unit)}
+
+Base.length(iter::VectorOption) = length(iter.values)
+
+Base.size(iter::VectorOption) = size(iter.values)
+
+Base.getindex(A::VectorOption, I) = getindex(A.values, I) * A.unit
+
+Base.setindex!(A::VectorOption, v, I) = setindex!(A.values, v, I)
+
 end
