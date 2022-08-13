cef446ad47bf5bd0a33758cedec8a27510e3a951
Reno <singularitti@outlook.com>
2022-08-12 23:03:12 -0400

    Move `distribute_procs` to src/actions.jl

--- src/ExpressWorkflowMaker.jl
+++ src/ExpressWorkflowMaker.jl
@@ -1,15 +1,5 @@
 module ExpressWorkflowMaker
 
-export distribute_procs
-
-function distribute_procs(nprocs, jobsize)
-    quotient, remainder = divrem(nprocs, jobsize)
-    if !iszero(remainder)
-        @warn "the processes are not fully balanced! Consider the number of subjobs!"
-    end
-    return quotient
-end
-
 include("Config.jl")
 include("actions.jl")
 include("Templates/Templates.jl")
--- src/actions.jl
+++ src/actions.jl
@@ -1,1 +1,11 @@
+export distribute_procs
+
+function distribute_procs(nprocs, jobsize)
+    quotient, remainder = divrem(nprocs, jobsize)
+    if !iszero(remainder)
+        @warn "the processes are not fully balanced! Consider the number of subjobs!"
+    end
+    return quotient
+end
+
 function jobify end
